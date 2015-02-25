3a248999d05d774353e501cc79d367edd6cdb2f7
Eduardo Jiménez <eduardojimenez_97@hotmail.com>
2015-02-25 10:38:26 -0600

    Tareas y practicas
    Se agrega la tarea y las practicas de la unidad 1

---
+++ EJO/practica2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Práctica 2</title>
+	</head>
+	<body>
+		<h1>
+			Listas
+		</h1>
+		<p>
+			Hay tres tipos de listas:
+			<ul>
+				<li><code>< ul ></code> - Lista desordenada</li>
+				<li><code>< ol ></code> - Lista ordenada </li>
+				<li><code>< dl ></code> - Lista definicion </li>
+			</ul>
+			<ol>
+				<li>Primero</li>
+				<li>Segundo</li>
+				<li>Tercero</li>
+			</ol>
+			<dl>
+				<dt>Elemento ul</dt>
+					<dd>
+						Sus principales atributos genericos son:<br>
+						<b>style:</b> define los estilos<br>
+						Sus principales atributos de transicion son:<br>
+						<b>type:</b> define el estilo de  viñetas, los cuales pueden ser disc (disco), circle (circulo) y square (cuadrado)
+					</dd>
+				<dt>Elemento ol</dt>
+					<dd>
+						
+					</dd>
+				<dt>Listas de definicion</dt>
+					<dd> sirve para mostrar definiciones</dd>
+			</dl>
+		</p>
+	</body>
+</html>
