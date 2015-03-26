a1bf9a2bea76d2156c8d7f09ce97f0f31b69bc02
mondro <francisco_mondragon@ucol.mx>
2015-03-26 09:07:21 -0600

    modificaciones

---
+++ FJGM/PracticaValidaciones11-03-15/bootstrap-3.3.2-dist/pract7/datos.php
@@ -0,0 +1,77 @@
+<?php
+	$nombre = $_POST['iNombre'];
+	$domicilio = $_POST['iDomicilio'];
+	$telefono = $_POST['iPhone'];
+?>
+
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+
+		<!-- Bootstrap CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<form action="datos.php" method="POST" role="form">
+				<h1 class="text-center">Datos</h1>
+				<table class="table table-hover">
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Domicilio</th>
+							<th>Télefono</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td><?php echo $nombre ?></td>
+							<td><?php echo $domicilio?></td>
+							<td><?php echo $telefono?></td>
+						</tr>
+					</tbody>
+				</table>
+        		<footer class="text-center">Francisco Javier Gutierrez Mondragon</footer>
+    		</form>
+		</div>
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
+
+<!--
+<?php
+	$nombre = "Francisco";
+	echo is_string($nombre);  //Regresara un 1 
+	$nombre = 25;			//Cambia el valor y el tipo de la variable
+	echo is_string($nombre); //Regresara un 0 por que cambio el tipo de variable de string a in
+
+	if (isset($_GET['tTarea']))
+		echo $_GET['tTarea'];
+	else
+		echo "la variable no extiste";
+	/*
+	echo (string)$nombre;
+	$_GET -> VARIABLES ENVIADAS POR METODO GET
+	$_POST -> VARIABLES ENVIADAS POR METODO POST
+	$_COOKIE -> ARRAY DE COOKIE
+	$_FILE -> VARIABLES DE TIPO ARCHIVO
+	$_SERVER -> HEADERS, RUTAS DE ARCHIVO, RUTAS DE SCRIPT
+	$_SESSION -> SESION REGISTRADA ACTUALMENTE
+	$_REQUEST -> GET,POST, SESSION, COOKIE
+	*/
+?>
+
+-->
---
+++ FJGM/PracticaValidaciones11-03-15/bootstrap-3.3.2-dist/pract7/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Practica 7</title>
+
+		<!-- Bootstrap CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container well">
+		<form action="datos.php" method="POST" role="form">
+			<h1>Registro de Usuario</h1>
+			<div class="form-group">
+				<label for="">Nombre</label>
+				<input type="text" class="form-control" name="iNombre" id="iNombre" placeholder="Nombre completo">
+				<label for="">Domicilio</label>
+				<input type="text" class="form-control" name="iDomicilio" id="iDomicilio" placeholder="Dommicilio completo">
+				<label for="">Télefono</label>
+				<input type="text" class="form-control" name="iPhone" id="iPhone" placeholder="(312) 104-8231">
+			</div>
+			<button type="submit" class="btn btn-primary">Registrar</button>
+            <footer class="text-center">Francisco Javier Gutierrez Mondragon</footer>
+		</div>
+		<!-- jQuery -->
+		<script src="js/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
